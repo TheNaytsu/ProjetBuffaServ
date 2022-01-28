cb312e4bc7ea261b0576d2179dd0c8363a8414ef
Yann S <56241227+TheNaytsu@users.noreply.github.com>
2022-01-28 22:35:30 +0100

    Update AuthController.js

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -38,7 +38,7 @@ router.post('/login', async (req, res) =>{
         const token = jwt.sign({ _id: user._id }, config.secret);
 
         res.cookie('jwt',token, {
-            httpOnly: true,
+            secure:true,
             maxAge: 24*60*60*1000
         })
 
